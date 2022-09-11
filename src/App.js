9ab72a8389bd38a1e1098f1a3a4f813c01c316d7
Iván Oliva <ivolicode@gmail.com>
2022-09-11 14:25:29 -0500

    Initial pokedex commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useApi } from './hooks/useApi';
+import { Paginator } from './components/Paginator';
+import { SearchBar } from './components/SearchBar';
+import { PokemonCard } from './components/PokemonCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+export default function App() {
+  const [pokemonListUrl, setPokemonListUrl] = useState('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0#');
+  const pokemonListData = useApi(pokemonListUrl)
+  const [pokemonUrl, setPokemonUrl] = useState('https://pokeapi.co/api/v2/pokemon/1/');
+  const pokemon = useApi(pokemonUrl)
+
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    let pokemon = e.target[0].value.toLowerCase()
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
+    setPokemonUrl(url)
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-5">
+      <Row >
+        <Col xs={4} md={2} className="mt-5">
+          <Paginator pokemonListData={pokemonListData.data} setPokemonListUrl={setPokemonListUrl} setPokemonUrl={setPokemonUrl} />
+        </Col>
+        <Col xs={8} md={10} >
+          <SearchBar handleSubmit={handleSubmit}/>
+          <PokemonCard pokemon={pokemon.data}/>
+        </Col>
+      </Row>
+    </Container>
   );
 }
-
-export default App;
